34a41040daae67d3124c44c9ba266b0c5cd14bf6
maxcabd <84046498+maxcabd@users.noreply.github.com>
2024-04-02 10:28:17 -0400

    Fix issue with incorrect costumeSlot index for CharacterSelectParam entries

--- src/param/add_entry.rs
+++ src/param/add_entry.rs
@@ -6,8 +6,8 @@ use nuccbin::{
     },
     NuccBinaryType,
 };
-use std::collections::HashMap;
 
+use std::collections::HashMap;
 use super::calc_crc32;
 
 pub fn add_message_info_entry(
@@ -30,15 +30,16 @@ pub fn add_message_info_entry(
             entry.text3 == costume.char_name && entry.crc32 == calc_crc32(&costume.cha_id)
         });
 
-        let base_entry = message_info
+        let name_entry = message_info
             .entries
             .iter_mut()
             .find(|entry| entry.crc32 == [246, 160, 24, 181]) // Some random crc32 value that exists for a costume name
             .unwrap();
 
-        let mut chara_name_entry = base_entry.clone();
+        let mut chara_name_entry = name_entry;
         chara_name_entry.crc32 = calc_crc32(&costume.cha_id);
         chara_name_entry.text3 = costume.char_name.clone();
+
 
         let mut costume_name_entry = chara_name_entry.clone();
         costume_name_entry.crc32 = calc_crc32(&costume.costume_id);
@@ -48,11 +49,10 @@ pub fn add_message_info_entry(
             continue;
         }
         entries.push(chara_name_entry.clone());
-
-        if !costume_name_exists || costume.costume_name.is_empty() {
+     
+        if costume_name_exists {
             continue;
         }
-        
         entries.push(costume_name_entry.clone());
 
     }
@@ -76,18 +76,29 @@ pub fn add_player_setting_entry(
         .max()
         .unwrap_or_default();
 
+        let mut sorted_costumes = cfg.costumes.clone();
+        sorted_costumes.sort_by(|a, b| {
+            a.characode
+                .cmp(&b.characode)
+                .then(a.model_index.cmp(&b.model_index))
+        });
 
-    for costume in cfg.costumes.iter() {
-        //let searchcode = format!("{}00", &costume.characode);
+    for costume in sorted_costumes.iter() {
+         let not_exist = player_setting.entries.iter().any(|entry| {
+            entry.cha_b_id == costume.cha_id
+                && entry.duel_player_param_model_index == costume.model_index
+        });
 
-        let base_entry = player_setting
+
+        let main_entry = player_setting
             .entries
             .iter_mut()
             .filter(|entry| entry.searchcode.contains(&costume.characode))
             .max_by_key(|entry| entry.player_setting_id)
             .unwrap();
 
-        let mut entry = base_entry.clone();
+        
+        let mut entry = main_entry.clone();
 
         entry.player_setting_id = highest_id + 1;
         entry.duel_player_param_model_index = costume.model_index;
@@ -102,49 +113,13 @@ pub fn add_player_setting_entry(
 
         highest_id += 1; // Increment the highest id for the next entry
 
+        // Only push if the entry doesn't already exist
+        if not_exist {
+            continue;
+        }
+
         player_setting.entries.push(entry);
     }
-
-    /*let entries = cfg
-        .costumes
-        .iter()
-        .filter_map(|costume| {
-            if player_setting
-                .entries
-                .iter()
-                .any(|entry| entry.cha_b_id == costume.cha_id)
-            {
-                None // Costume entry already exists, skip
-            } else {
-                let base_entry = player_setting
-                    .entries
-                    .iter_mut()
-                    .filter(|entry| entry.searchcode.contains(&costume.characode))
-                    .max_by_key(|entry| entry.player_setting_id)?;
-                
-                println!("{:#?}", base_entry);
-
-                let mut entry = base_entry.clone();
-
-                entry.player_setting_id = highest_id + 1;
-                entry.duel_player_param_model_index = costume.model_index;
-
-                entry.searchcode = format!(
-                    "{}{:02}",
-                    &entry.searchcode.chars().take(4).collect::<String>(),
-                    entry.searchcode.chars().nth(5)?.to_digit(10)? + 1
-                );
-
-                entry.cha_b_id = costume.cha_id.clone();
-
-                highest_id += 1; // Increment the highest id for the next entry
-
-                Some(entry)
-            }
-        })
-        .collect::<Vec<_>>();*/
-
-    //player_setting.entries.extend(entries);
 }
 
 pub fn add_costume_entry(
@@ -184,15 +159,13 @@ pub fn add_costume_entry(
         .unwrap_or(0)
         + 10;
 
-    // before we start, we should order the cfg.costumes by the characode
+    // Sort the costumes so we add the entries in the correct order
     let mut sorted_costumes = cfg.costumes.clone();
     sorted_costumes.sort_by(|a, b| {
         a.characode
             .cmp(&b.characode)
             .then(b.model_index.cmp(&a.model_index))
     });
-
-    //let mut color_count = 0;
 
     for costume in sorted_costumes.iter() {
         let characode_index = player_setting
@@ -203,7 +176,6 @@ pub fn add_costume_entry(
             .next()
             .unwrap();
 
-        // now get the base psp entry using the characode_index
         let psp_entry = player_setting
             .entries
             .iter()
@@ -212,16 +184,16 @@ pub fn add_costume_entry(
             .unwrap();
 
         // We only need the base costume entry to get the index to insert the new entries after it
-        let base_cos_entry = entries_clone
+        let main_entry = entries_clone
             .iter()
             .filter(|entry| entry.player_setting_id == psp_entry.player_setting_id)
             .max_by_key(|entry| entry.color_index)
             .unwrap()
             .clone(); // Clone to avoid borrowing
 
-        let base_entry_index = entries_clone
+        let main_entry_index = entries_clone
             .iter()
-            .position(|entry| entry == &base_cos_entry)
+            .position(|entry| entry == &main_entry)
             .unwrap();
 
         // We need to find our new psp id we added in the player_setting_param
@@ -234,7 +206,7 @@ pub fn add_costume_entry(
             .unwrap();
 
         for i in 0..costume.color_count {
-            let mut cos_entry = base_cos_entry.clone();
+            let mut cos_entry = main_entry.clone();
             cos_entry.player_setting_id = highest_psp_id;
             cos_entry.color_index = i as u32;
 
@@ -255,7 +227,7 @@ pub fn add_costume_entry(
             cos_entry.costume_link =
                 format!("COSTUME_{:05}", highest_costume_link + (10 * i as u32));
 
-            entries_clone.insert(base_entry_index + 1 + i as usize, cos_entry);
+            entries_clone.insert(main_entry_index + 1 + i as usize, cos_entry);
         }
 
         highest_costume_link += 10 * costume.color_count as u32;
@@ -286,26 +258,30 @@ pub fn add_icon_entry(
     let mut entries = Vec::new();
 
     for costume in cfg.costumes.iter() {
-        let searchcode = format!("{}00", &costume.characode);
-
-        // Find the base psp entry to get the psp id for the costume entry
-        let psp_entry = player_setting
+        let latest_psp_entry = player_setting
             .entries
             .iter()
-            .filter(|entry| entry.searchcode == searchcode)
+            .filter(|entry| entry.searchcode.contains(&costume.characode))
             .min_by_key(|entry| entry.player_setting_id)
             .unwrap();
 
-        let characode_index = psp_entry.characode_index;
+        let characode_index = latest_psp_entry.characode_index;
 
-        let base_entry = player_icon
+        let main_entry = player_icon
             .entries
             .iter_mut()
             .filter(|entry| entry.characode_index == characode_index)
             .max_by_key(|entry| entry.duel_player_param_costume_index)
             .unwrap();
 
-        let mut icon_entry = base_entry.clone();
+        let mut entry = main_entry.clone();
+
+        if costume.iconcode.is_empty() {
+            continue;
+        }
+
+        entry.icon_id = costume.iconcode.clone();
+        entry.duel_player_param_costume_index = costume.model_index;
 
         let not_exist = player_icon.entries.iter().any(|entry| {
             entry.duel_player_param_costume_index == costume.model_index
@@ -316,10 +292,8 @@ pub fn add_icon_entry(
             continue;
         }
 
-        icon_entry.icon_id = costume.iconcode.clone();
-        icon_entry.duel_player_param_costume_index = costume.model_index;
 
-        entries.push(icon_entry.clone());
+        entries.push(entry.clone());
     }
 
     player_icon.entries.extend(entries);
@@ -329,6 +303,16 @@ pub fn add_character_select_entry(
     nucc_binaries: &mut HashMap<NuccBinaryType, Box<dyn NuccBinaryParsed>>,
     cfg: &CostumeAddConfig,
 ) {
+    let player_setting = {
+        let player_setting_ref = nucc_binaries
+            .get(&NuccBinaryType::PlayerSettingParam)
+            .unwrap()
+            .downcast_ref::<PlayerSettingParam>()
+            .unwrap();
+        player_setting_ref.clone() // Clone the reference
+    };
+
+
     let character_select = nucc_binaries
         .get_mut(&NuccBinaryType::CharacterSelectParam)
         .unwrap()
@@ -338,47 +322,56 @@ pub fn add_character_select_entry(
     let mut entries = Vec::new();
 
     for costume in cfg.costumes.iter() {
-        //let searchcode = format!("{}00", &costume.characode);
-        let base_entry = character_select
+        // This is our playerSetting entry that we just added for the costume
+        let latest_psp_entry = player_setting
             .entries
-            .iter_mut()
-            .filter(|entry| entry.searchcode.contains(&costume.characode))
+            .iter()
+            .filter(|entry| entry.cha_b_id == costume.cha_id && entry.duel_player_param_model_index == costume.model_index)
+            .min_by_key(|entry| entry.player_setting_id)
+            .unwrap();
+
+        let main_psp_entry = player_setting
+            .entries
+            .iter()
+            .filter(|entry| entry.characode_index == latest_psp_entry.characode_index)
+            .min_by_key(|entry| entry.player_setting_id)
+            .unwrap();
+
+
+        let main_base_entry = character_select
+            .entries
+            .iter()
+            .filter(|entry| entry.searchcode == main_psp_entry.searchcode.clone())
+            .min_by_key(|entry| entry.costume_slot_index)
+            .unwrap();
+
+        
+        let latest_base_entry = character_select
+            .entries
+            .iter()
+            .filter(|entry| entry.page_index == main_base_entry.page_index && entry.slot_index == main_base_entry.slot_index)
             .max_by_key(|entry| entry.costume_slot_index)
             .unwrap();
 
-        let mut entry = base_entry.clone();
+        
+        let mut entry = latest_base_entry.clone();
 
-        entry.costume_slot_index = entry.costume_slot_index + 1;
-        entry.searchcode = format!(
-            "{}{}",
-            &entry.searchcode.chars().take(4).collect::<String>(),
-            format!(
-                "{:02}",
-                entry
-                    .searchcode
-                    .chars()
-                    .nth(5)
-                    .unwrap()
-                    .to_digit(10)
-                    .unwrap()
-                    + 1
-            )
-        );
-
+        entry.costume_slot_index = costume.model_index as u32;
+        entry.searchcode = latest_psp_entry.searchcode.clone();
         entry.costume_name = costume.costume_id.clone();
-        entry.dictionary_link = "".to_string();
-
+        
         let not_exist = character_select.entries.iter().any(|e| {
             e.costume_name == costume.costume_id
                 && e.page_index == entry.page_index
                 && e.slot_index == entry.slot_index
+                && e.searchcode == entry.searchcode
         });
 
         if not_exist {
             continue;
         }
-
-        entries.push(entry.clone());
+        
+        entries.push(entry);
     }
 
     character_select.entries.extend(entries);
@@ -406,8 +399,7 @@ pub fn add_costume_break_entry(
     let mut entries = Vec::new();
 
     for costume in cfg.costumes.iter() {
-        if !costume.has_costume_break {
-            // if has_costume_break is true, we add a new entry
+        if costume.has_costume_break { // Only add costume break entries if set to true
             continue;
         }
 
@@ -422,15 +414,15 @@ pub fn add_costume_break_entry(
 
         let characode_index = psp_entry.characode_index;
 
-        let base_entry = costume_break
+        let main_entry = costume_break
             .entries
             .iter_mut()
             .filter(|entry| entry.characode_index == characode_index)
             .min_by_key(|entry| entry.costume_index)
             .unwrap();
 
-        let mut cos_break_entry = base_entry.clone();
-        cos_break_entry.costume_index = costume.model_index as u32;
+        let mut entry = main_entry.clone();
+        entry.costume_index = costume.model_index as u32;
 
         let not_exist = costume_break.entries.iter().any(|entry| {
             entry.costume_index == costume.model_index as u32
@@ -441,7 +433,7 @@ pub fn add_costume_break_entry(
             continue;
         }
 
-        entries.push(cos_break_entry.clone());
+        entries.push(entry.clone());
     }
 
     costume_break.entries.extend(entries);
